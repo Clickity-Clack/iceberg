a53f6b59ada720a9b631c0f2b11bcd47b8165180
David Germain <davidispizza@gmail.com>
2018-02-19 21:46:04 -0500

    send static reply

--- public/client.js
+++ public/client.js
@@ -1,3 +1,24 @@
 $(document).ready(() => {
-  var socket = io()
+
+  const socket = io()
+  const ENTER_BUTTON = $('#enter')
+  let IS_GAME_ASKING_FOR_INPUT = false
+
+  socket.on('poll', (gameState) => {
+    console.log(gameState)
+    IS_GAME_ASKING_FOR_INPUT = true
+  })
+
+  ENTER_BUTTON.click(() => {
+    console.log('click')
+    if (IS_GAME_ASKING_FOR_INPUT) {
+      const placement = {
+        message: 'goodbye'
+      }
+      console.log('sending?')
+      socket.emit('answer', placement)
+      IS_GAME_ASKING_FOR_INPUT = false;
+    }
+  })
+
 })

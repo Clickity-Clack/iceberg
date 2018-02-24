d59dbe832c06e75b94cf92edf1fb61f59ae78e08
David Germain <davidispizza@gmail.com>
2018-02-23 22:17:57 -0500

    add expect game states

--- test/game.spec.js
+++ test/game.spec.js
@@ -8,19 +8,22 @@ const Polyomino = require('../game/polyomino')
 const Coordinate = require('../game/coordinate')
 
 describe('game', () => {
-  let mockPlayer1
+  let sandbox = sinon.createSandbox()
+  let player1
+  let updateGameStatePlayer1Stub
   let game
   let defaultPlacement
   let STANDARD_CONFIGURATION = {
     BOARD_SIZE: 14,
     PLAYERS: [
-      mockPlayer1
+      player1
     ]
   }
 
   describe('place', () => {
     beforeEach(() => {
-      mockPlayer1 = new Player()
+      player1 = new Player('A')
+      updateGameStatePlayer1Stub = sinon.stub(player1, 'updateGameState')
       game = Game.make(STANDARD_CONFIGURATION)
       defaultPlacement = Placement.make(
         Polyomino.NAME.I1,
@@ -28,24 +31,72 @@ describe('game', () => {
         Placement.ROTATION.MIDNIGHT,
         Placement.SIDE.A
       )
+
+      let startingGamestate = {
+        "placements" : [],
+        "players" : {
+          "1" : {
+            "polyominoes" :[ "O4", "I1", "Y", "P" ],
+            "score" : 0
+          },
+          "2" : {
+            "polyominoes" :[ "O4", "I1", "Y", "P" ],
+            "score" : 0
+          },
+        },
+        "turnRotation" : [ "1", "2" ],
+        "whoseTurn" : "1"
+      }
+
+      let gameStateAfterOneTurn = {
+        "placements" : [{
+          "name" : "I1",
+          "coordinate" : {
+              "x" : 0,
+              "y" : 0
+          }, 
+          "roation" : "MIDNIGHT",
+          "side" : "A"
+        }],
+        "players" : {
+          "1" : {
+            "polyominoes" :[ "O4", "Y", "P" ],
+            "score" : 123
+          },
+          "2" : {
+            "polyominoes" :[ "O4", "I1", "Y", "P" ],
+            "score" : 123
+          },
+        },
+        "turnRotation" : [ "1", "2" ],
+        "whoseTurn" : "2"
+      }
     })
+
+    it('empty board is correct', () => {
+      assert(game.gamestate == startingGamestate)
+    })    
 
     it('gamestate changes after placement', () => {
       let startingGamestate = game.gamestate
-      game.place(mockPlayer1, defaultPlacement)
+      game.place(player1, defaultPlacement)
       assert(game.gamestate != startingGamestate)
     })
 
     it('GameState\'s board property updates properly', () => {
-      let expectedBoard = [defaultPlacement]
-      game.place(mockPlayer1, defaultPlacement)
-      assert(game.gamestate.board == expectedBoard)
+      game.place(player1, defaultPlacement)
+      assert(game.gamestate == gameStateAfterOneTurn) // TODO: do we use '==' ? 
     })
 
     it('pushes new gamestate to player after placement', () => {
-      sinon.spy(mockPlayer1, "updateGameState")
-      game.place(mockPlayer1, defaultPlacement)
-      assert(mockPlayer1.updateGameState.calledOnce)
+      sinon.spy(player1, "updateGameState")
+      game.place(player1, defaultPlacement)
+      assert(player1.updateGameState.calledOnce)
+    })
+
+    it('pushes new gamestate to player after placement', () => {
+      game.place(player1, defaultPlacement)
+      assert(updateGameStatePlayer1Stub.withArgs(gameStateAfterOneTurn))
     })
   })
 })
